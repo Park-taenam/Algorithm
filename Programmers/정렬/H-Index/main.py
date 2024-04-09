581d8cb4893d035219a0888cd93ecaa1a344b9bf
Park-taenam <taenam@inha.edu>
2024-04-09 15:37:21 +0900

    Add: Programmers H-Index

---
+++ Programmers/정렬/H-Index/main.py
@@ -0,0 +1,11 @@
+def solution(citations):
+    answer = 0
+    citations.sort(reverse=True)
+    
+    for i, num in enumerate(citations):
+        if num > i:         # i or answer
+            answer = i+1
+        else:
+            break
+    
+    return answer
